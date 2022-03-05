f89d0d3fcd66ddb741436f3d15f74c1d8c2edb74
Audrey LOURI <audrey.louri@gmail.com>
2022-03-05 18:38:20 +0100

    add link on header text

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { HiOutlineMoon } from 'react-icons/hi';
 
 import './style.scss';
@@ -5,9 +6,11 @@ import './style.scss';
 const Header = () => {
   return (
     <header className="header">
-      <div>
-        <h2 className="header__title">Where in the world?</h2>
-      </div>
+      <Link to="/">
+        <div>
+          <h2 className="header__title">Where in the world?</h2>
+        </div>
+      </Link>
       <div className="header__mode">
         <HiOutlineMoon />
         <button 
